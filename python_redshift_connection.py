6f77ae005d80406059cff887e813e4ff2a16957f
vrreddy695 <ramireddy2006@gmail.com>
2018-06-25 22:14:25 -0700

    Update python_redshift_connection.py
    mask the password

--- python_redshift_connection.py
+++ python_redshift_connection.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 import psycopg2
-engine = create_engine('redshift://shifu:2Lvgoofy@scooby.cbbirzec93nx.us-east-1.redshift.amazonaws.com:5439/biscuit')
+engine = create_engine('redshift://shifu:*********@scooby.cbbirzec93nx.us-east-1.redshift.amazonaws.com:5439/biscuit')
 
 df1.to_sql('vr_GetShoppingSources_06_17', engine, schema='temp', index = False, chunksize=10000)
